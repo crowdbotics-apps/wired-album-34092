e31c701cd3301d6f319e23960ccbfbb9704052af
Crowdbotics <team@crowdbotics.com>
2022-03-10 03:20:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "wired_album_34092",
+  url: "https://wired_album_34092.botics.co",
+  api: "https://wired_album_34092.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
